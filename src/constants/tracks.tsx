56a9f5a3c0358748e53f62ec50a7e7660265b6e2
sureoi25 <juddplug@gmail.com>
2025-07-29 13:09:35 +0800

    Chore: Try fix music

--- src/constants/tracks.tsx
+++ src/constants/tracks.tsx
@@ -1,30 +1,30 @@
 export const TRACKS = {
   LANDING: {
-    url: 'audio/Streetcar.mp3',
+    url: '/gallery/audio/Streetcar.mp3',
     title: 'Streetcar Daniel Caesar'
   },
   PHYSICAL: {
-    url: 'audio/G.O.M.D.mp3',
+    url: '/gallery/audio/G.O.M.D.mp3',
     title: 'GOMD J. Cole'
   },
   MATERIAL: {
-    url: 'audio/beaches.mp3', 
+    url: '/gallery/audio/beaches.mp3', 
     title: 'Beaches Beabadoobee'
   },
   SEXUAL: {
-    url: 'audio/With you.mp3',
+    url: '/gallery/audio/With you.mp3',
     title: 'With you Chris Brown'
   },
   POLITICAL: {
-    url: 'audio/The bigger picture.mp3',
+    url: '/gallery/audio/The bigger picture.mp3',
     title: 'The Bigger Picture Lil Baby'
   },
   SPIRITUAL: {
-    url: 'audio/Our father.mp3',
+    url: '/gallery/audio/Our father.mp3',
     title: 'Our Father Don Moen'
   },
   DIGITAL: {
-    url: 'audio/cat.mp3',
+    url: '/gallery/audio/cat.mp3',
     title: 'Cat brainrot'
   }
 };
