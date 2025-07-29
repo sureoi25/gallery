c15d8e4d850b6b8fddc2c32c81e846951410d871
sureoi25 <juddplug@gmail.com>
2025-07-29 18:51:47 +0800

    fix some grammatical errors in materialpage

--- src/pages/materialSelf.tsx
+++ src/pages/materialSelf.tsx
@@ -37,15 +37,15 @@ const MaterialSelf: React.FC = () => {
           </p>
 
           <p className="mb-4">
-            Reading Dostoevsky works is a recent addition to my repertoire. During my highschool years, I would
+            Reading Dostoevsky's works is a recent addition to my repertoire. During my highschool years, I would
             pick up a few books here and there, like Mitch Albom and Percy Jackson books, but recently I have been influenced 
-            by a friend to read Dostoevsky, and have been enjoying it ever since. It represents how I am still growing and learning to love new things.<br/>
+            by a friend to read Dostoevsky and have been enjoying it ever since. It represents how I am still growing and learning to love new things.<br/>
           </p>
 
           <p className="mb-4">
             My computer and laptop are the quintessential tools of my computer science student/gamer self. Although now, I have since relegated the duty of gaming to my computer only, and reserved work for my laptop.
-            My laptop is a recent acquisition because I kept getting distracted when I try to work on my computer and also since portability was much more of a need than I first thought.
-            I think that they are the best representation of my passion for software engineering, and my love for gaming. The charmander lego on my desk is also something my girlfriend and I built together in one of our dates. 
+            My laptop is a recent acquisition because I kept getting distracted when I try to work on my computer, and portability was much more of a need than I first thought.
+            I think that they are the best representation of my passion for software engineering and my love for gaming. The charmander lego on my desk is also something my girlfriend and I built together in one of our dates. 
             The keyboard is a gift from my sister or rather an acquisition from her, because I convinced her to give it to me HHAHAA.
           </p>
 
